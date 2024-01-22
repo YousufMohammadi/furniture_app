a8c9174dcac27d3516a233fd6a32833f54d049e5
Yousuf Mohammadi <theehsansarshar@Yousufs-MacBook-Air.local>
2024-01-22 12:12:29 +0430

    bottom navigation component created

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
+import BottomTabNavigation from "./navigation/BottomTabNavigation";
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   const [fontsLoaded] = useFonts({
-    regular: "./assets/fonts/Poppins-Regular.ttf",
-    light: "./assets/fonts/Poppins-Light.ttf",
-    bold: "./assets/fonts/Poppins-Bold.ttf",
-    medium: "./assets/fonts/Poppins-Medium.ttf",
-    semiBold: "./assets/fonts/Poppins-SemiBold.ttf",
-    extraBold: "./assets/fonts/Poppins-ExtraBold.ttf",
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    semiBold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+    extraBold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
   });
   const onLayoutRootView = useCallback(async()=>{
     if (fontsLoaded) {
@@ -20,11 +25,27 @@ export default function App() {
       return null;
     }
   },[fontsLoaded]);
+  // useEffect(() => {
+  //   async function prepare() {
+  //     await SplashScreen.preventAutoHideAsync();
+  //   }
+  //   prepare();
+  // }, []);
+  // if (!fontsLoaded) {
+  //   return undefined;
+  // } else {
+  //   SplashScreen.hideAsync;
+  // }
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>Project Structure created !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="bottomNavigation"
+          component={BottomTabNavigation}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Cart = () => {
+const Home = () => {
   return (
     <View>
-      <Text>Cart</Text>
+      <Text>Home</Text>
     </View>
   )
 }
 
-export default Cart
+export default Home
